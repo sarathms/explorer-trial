efbb54b751909b36ed610f27dd3de13ed0ac64cd
Sarath M S <sarath.ms@gmail.com>
2021-06-14 12:27:22 -0400

    Configured for explorer-trial project

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,14 +1,14 @@
-// This file configures the intialization of Sentry on the browser.
+// This file configures the initialization of Sentry on the browser.
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs'
+import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: SENTRY_DSN || 'https://d83de54688c84dcea72a45a98d43415a@o155150.ingest.sentry.io/5816004',
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-})
+});
