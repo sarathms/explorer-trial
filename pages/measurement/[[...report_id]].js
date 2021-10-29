7ed2d8796b9f40fa44740f6fb8e9ee103d3c0c81
Sarath M S <sarath.ms@gmail.com>
2021-10-29 13:16:51 -0400

    getServersideProps with throw

--- pages/measurement/[[...report_id]].js
+++ pages/measurement/[[...report_id]].js
@@ -8,6 +8,7 @@ const MeasurementPage = (props) => {
 }
 
 export async function getServerSideProps({ query }) {
+  throw new Error('error in getServerSideProps')
   console.log('getServerSideProps', query)
   return {
     props: {
