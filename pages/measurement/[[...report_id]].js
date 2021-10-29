5c0060da5225a3c3584ed11d1e25109f7ba3304a
Sarath M S <sarath.ms@gmail.com>
2021-10-29 13:23:03 -0400

    replicate explorer getserversidepropds

--- pages/measurement/[[...report_id]].js
+++ pages/measurement/[[...report_id]].js
@@ -4,15 +4,28 @@ const MeasurementPage = (props) => {
   console.log('Does this even run server side?')
   console.log('props', props)
   console.log(`report_id: ${props.report_id}`)
-  return (<div> On preview branch { props.report_id } </div>)
+  return (<div> On preview branch { JSON.stringify(props, null, 2)} </div>)
 }
 
 export async function getServerSideProps({ query }) {
-  throw new Error('error in getServerSideProps')
-  console.log('getServerSideProps', query)
-  return {
-    props: {
-      report_id: query.report_id[0]
+  let initialProps = {
+    errors: [],
+    ssrRequests: {}
+  }
+  const report_id = query?.report_id?.[0]
+  if (typeof report_id !== 'string' || report_id.length < 1) {
+    initialProps.notFound = true
+    initialProps.ssrRequests = {
+      message: `failed early. could not find a report_id: ${report_id} (${typeof report_id})`
+    }
+    return {
+      props: initialProps
+    }
+  } else {
+    initialProps.report_id = report_id
+    initialProps.letssee = 'test'
+    return {
+      props: initialProps
     }
   }
 }
